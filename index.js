441e9a925bb62ee1773513a8f09b8b8f9f9ffbf8
Thomas Y <corragon@gmail.com>
2018-05-17 14:46:40 -0500

    Move App.js to js folder

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import App from './js/App';
 AppRegistry.registerComponent('KeepContact', () => App);
 //Hack to silence bug with React Native/React Navigation
 // https://github.com/react-navigation/react-navigation/issues/3956
